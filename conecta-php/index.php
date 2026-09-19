b953b4e4b59641ae03e4fd51db0fe230e4fe17cf
Samuelzin-Da-XJ <carlosrobertomedina11@gmail.com>
2026-09-18 22:24:50 -0300

    primeiro commit

---
+++ conecta-php/index.php
@@ -0,0 +1,15 @@
+<?php
+include "valida_cookie.inc";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validação de Cookie</title>
+</head>
+<body>
+    Seja bem-vindo ao meu site<br>
+    Coloque o conteúdo do seu site aqui neste espaço<br>
+</body>
+</html>
---
+++ conecta-php/logout.php
@@ -0,0 +1,5 @@
+<?php
+    Setcookie("email_usuario");
+    Setcookie("senha_usuario");
+    header("location: login.html");
+?>
